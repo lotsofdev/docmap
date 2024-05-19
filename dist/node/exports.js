849729c8a70a06e73094d782b8b373b24eb0e8f2
Olivier Bossel <olivier.bossel@gmail.com>
2024-05-19 22:47:50 +0200

    up

--- dist/node/exports.js
+++
@@ -1,6 +0,0 @@
-import Docmap from './Docmap.js';
-import __defaults from './defaults.js';
-export * from './Docmap.js';
-export { __defaults };
-export default Docmap;
-//# sourceMappingURL=exports.js.map
