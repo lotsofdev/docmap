0ef79fbd17373c0ad31b5e7249a4514ad8800311
Olivier Bossel <ob@buzzbrothers.ch>
2024-08-12 17:47:54 +0200

    up

---
+++ dist/node/_exports.js
@@ -0,0 +1,7 @@
+import Docmap from './Docmap.js';
+import __defaults from './defaults.js';
+export * from './Docmap.js';
+export * from './types.js';
+export { __defaults };
+export default Docmap;
+//# sourceMappingURL=_exports.js.map
